82b3a88798b4a91538a7d04df15a78939505bf86
rzshrote <rzshrote@gmail.com>
2024-11-15 14:01:40 -0500

    add naive method

--- pynds/relation.py
+++ pynds/relation.py
@@ -39,41 +39,34 @@ def dominance_relationship(x: numpy.ndarray, y: numpy.ndarray) -> int:
     else:
         return 0
 
-# def maximizing_dominance_relationship(x: numpy.ndarray, y: numpy.ndarray) -> int:
-#     """
-#     Determine the dominance relationship between two vectors.
-#     Assumes that objectives are to be maximize.
-#     Assumes that all objective values are numbers or infinity.
-#     NaN values will not be handled correctly.
+def dominance_relationship_matrix(x: numpy.ndarray, dom: numpy.ndarray) -> None:
+    """
+    Calculate the dominance relationships between all rows in a matrix.
 
-#     Parameters
-#     ----------
-#     x : numpy.ndarray
-#         First vector for which to determine the dominance relationship.
-#     y : numpy.ndarray
-#         Second vector for which to determine the dominance relationship.
+    Parameters
+    ----------
+    x : numpy.ndarray
+        A matrix of shape ``(nindiv,nobj)`` containing objective function values.
+        Input matrix.
+    dom : numpy.ndarray
+        A matrix of shape ``(nindiv,nindiv)`` to store dominance relationships.
+        Output matrix.
+        If x[i] dominates x[j],           then dom[i,j] == -1.
+        If x[i] is non-dominated by x[j], then dom[i,j] == 0.
+        If x[i] is dominated by x[j],     then dom[i,j] == 1.
+    """
+    # get number of individuals
+    nindiv = x.shape[0]
+
+    # get expected and observed shapes
+    eshape = (nindiv,nindiv)
+    dshape = dom.shape
+
+    # test output size
+    if eshape != dshape:
+        raise ValueError("Output matrix ``dom`` is not the correct shape: expected ``{0}`` but received ``{1}``".format(eshape,dshape))
     
-#     Returns
-#     -------
-#     out : int
-#         If x dominates y, then return -1.
-#             (x < y) == -1
-#         If x is non-dominated by y, then return 0.
-#             (x !< y and y !< x) == 0
-#         If x is dominated by y, then return 1.
-#             (x > y) == 1
-#     """
-#     x_ge_y = numpy.all(x >= y)
-#     y_ge_x = numpy.all(x <= y)
-#     x_gt_y = numpy.any(x > y)
-#     y_gt_x = numpy.any(x < y)
-
-#     x_dom_y = x_ge_y and x_gt_y
-#     y_dom_x = y_ge_x and y_gt_x
-
-#     if x_dom_y:
-#         return -1
-#     elif y_dom_x:
-#         return 1
-#     else:
-#         return 0
+    # calculate dominance relationships
+    for i in range(nindiv):
+        for j in range(nindiv):
+            dom[i,j] = dominance_relationship(x[i], x[j])
--- test/test_naive.py
+++ test/test_naive.py
@@ -1,8 +1,12 @@
 import pytest
 import numpy
-from pynds.naive import krange, ndsort_naive
+from pynds.naive import krange, ndsort_naive1, ndsort_naive2
 from matplotlib import pyplot
 from pynds.relation import dominance_relationship
+
+################################################################################
+################################ Test Fixtures #################################
+################################################################################
 
 @pytest.fixture
 def nindiv():
@@ -20,13 +24,29 @@ def xmat(nindiv, nobj):
 def rankvec(nindiv):
     yield numpy.empty(nindiv, dtype = int)
 
+@pytest.fixture
+def dommat(nindiv):
+    yield numpy.empty((nindiv,nindiv), dtype = int)
+
+@pytest.fixture
+def remvec(nindiv):
+    yield numpy.empty(nindiv, dtype = int)
+
+@pytest.fixture
+def maskvec(nindiv):
+    yield numpy.empty(nindiv, dtype = bool)
+
+################################################################################
+################################## Unit Tests ##################################
+################################################################################
+
 def test_krange():
     exclude = 5
     for i in krange(0, exclude + 5, exclude):
         assert i != exclude
 
 def test_ndsort_naive_inplace_True(xmat, rankvec):
-    out = ndsort_naive(xmat, rankvec, inplace = True)
+    out = ndsort_naive1(xmat, rankvec, inplace = True)
     assert id(out) == id(xmat)
     assert out.shape == xmat.shape
 
@@ -59,7 +79,7 @@ def test_ndsort_naive_inplace_True(xmat, rankvec):
     pyplot.close()
 
 def test_ndsort_naive_inplace_False(xmat, rankvec):
-    out = ndsort_naive(xmat, rankvec, inplace = False)
+    out = ndsort_naive1(xmat, rankvec, inplace = False)
     assert id(out) != id(xmat)
     assert out.shape == xmat.shape
 
@@ -90,3 +110,37 @@ def test_ndsort_naive_inplace_False(xmat, rankvec):
     pyplot.scatter(out[:,0], out[:,1], c=rankvec, cmap='tab20')
     pyplot.savefig("test_naive_inplace_False.png")
     pyplot.close()
+
+
+def test_ndsort_naive2(xmat, rankvec, dommat, remvec, maskvec):
+    ndsort_naive2(xmat, rankvec, dommat, remvec, maskvec)
+
+    # list the number of unique non-dominated frontiers
+    nduniq = numpy.unique(rankvec)
+
+    # for each front, assert that no other point dominates another point (all non-dominate each other)
+    for front in nduniq:
+        mask = (rankvec == front)
+        ix = numpy.flatnonzero(mask)
+        for i in ix:
+            for j in ix:
+                assert dominance_relationship(xmat[i,:], xmat[j,:]) == 0
+
+    # for each successive front, assert that no member in the better front is dominated by any member in the worse front
+    for fronti,frontj in zip(nduniq[:len(nduniq)-1], nduniq[1:]):
+        maski = (rankvec == fronti)
+        maskj = (rankvec == frontj)
+        ixi = numpy.flatnonzero(maski)
+        ixj = numpy.flatnonzero(maskj)
+        for i in ixi:
+            for j in ixj:
+                # -1 == (x dom y); 0 == (x nondom y); 1 == (y dom x)
+                # no point in the worse front should dominate any point in the better front
+                assert dominance_relationship(xmat[i,:], xmat[j,:]) <= 0
+
+    # make figure for visual inspection
+    pyplot.scatter(xmat[:,0], xmat[:,1], c=rankvec, cmap='tab20')
+    pyplot.savefig("test_naive2.png")
+    pyplot.close()
+
+    print(numpy.concatenate([rankvec[:,None],xmat], axis = 1))
